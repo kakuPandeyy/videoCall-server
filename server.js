6928ce0b7eeaa41480c4e649ed751c461281c720
abhishek pandey <kakupandey2017@gmail.com>
2025-08-24 23:50:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,149 @@
+const http = require('http')
+const express = require('express')
+const {Server } = require('socket.io')
+const { error } = require('console')
+require('dotenv').config()
+
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // React frontend URL
+    methods: ["GET", "POST"]
+  }
+})
+
+
+const rooms =  new Map()
+const offerMap = new Map()
+
+function validation(arr,user) {
+
+  const mySet = new Set()
+  arr.forEach(element => {
+    mySet.add(element.username)
+  });
+
+    const res = [...mySet]
+    console.log(res)
+
+    const exist = res.includes(user)
+
+    console.log("exzi",exist,user)
+
+  if(res.length<2 && !exist ){
+     return true
+  }else{
+     
+     return false
+  }
+
+}
+
+// function vaidUserName (username){
+//  const exist = userMap.has(username)
+//  return !exist
+// }
+
+// function validationOfRoom(roomId,user) {
+
+//   const existsOfRoom = rooms.get(roomId)
+
+//   if (existsOfRoom) {
+
+//      existsOfRoom.forEach(element => {
+//       const {username} = element
+
+//       console.log(username,user)
+//       if ( user=== username) {
+//         console.log(user===username,user,username)
+//         return false
+//       }
+//      });
+
+//      return true
+//   }
+// }
+
+
+// function removeEntryByValue(map, valueToRemove) {
+//   for (let [key, value] of map.entries()) {
+//     if (value === valueToRemove) {
+//       map.delete(key);
+//       return true; // found and removed
+//     }
+//   }
+//   return false; // not found
+// }
+
+
+io.on("connection",socket=>{
+
+  console.log("connecting to ",socket.id)
+  
+  socket.on("join-room",({roomId,username})=>{
+
+    console.log("working join room")
+  
+    socket.data.roomId = roomId
+    socket.data.username = username
+  
+  
+     console.log("inti",rooms)
+
+    if (rooms.has(roomId)) {
+    let socketArry = rooms.get(roomId) 
+   
+  
+    if (validation(socketArry,username)) {
+      socketArry.push({socketId:socket.id,username:username}) 
+    }else{
+      socket.emit("room-full")
+      return
+    }
+    }else{
+      console.log("elsee",roomId)
+    rooms.set(roomId,[{socketId:socket.id,username:username}])
+
+    }
+ 
+  
+     
+    const hostUser = rooms.get(roomId).find(ele =>ele.socketId !==socket.id)
+    // console.log("117",hostUser)
+    if (hostUser) {
+      socket.emit("host-user",hostUser.socketId)
+      socket.to(hostUser.socketId).emit("joined-user",socket.id)
+    }
+
+    console.log(rooms)
+
+  })
+  
+  socket.on("offer",payload =>{
+
+  
+    io.to(payload.target).emit("offer",payload)
+  })
+  socket.on("answer",payload=>{
+    io.to(payload.target).emit("answer",payload)
+  })
+  socket.on("ice-candidate",incoming=>{
+    io.to(incoming.target).emit("ice-candidate",incoming.candidate)
+  })
+
+   socket.on("disconnect", () => {
+
+    const { roomId, username } = socket.data || {};
+    console.log("disconneted",socket.id)
+    console.log(rooms,socket.id,roomId,username)
+    rooms.delete(roomId)
+
+  });
+})
+
+ 
+server.listen(process.env.PORT,()=>{
+  console.log(" server is runing at",process.env.PORT)
+})
